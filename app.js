4e5a6c111672c66e3da61a82bfb26690e96bf113
mannonov0830 <kamronbekmannonov0830@gmail.com>
2026-04-26 11:47:56 +0500

    main

--- app.js
+++ app.js
@@ -3,3 +3,15 @@ let display = document.getElementById("display")
 function bos(velue) {
     display.velue += velue
 }
+
+function hisoblash(params) {
+    let qiymat = display.velue
+
+    let son = "";
+    let natija = null;
+    let amal = null;
+
+    for (let i = 0; i < qiymat.length; i++) {
+        let belgi = qiymat[i]
+    }
+}
