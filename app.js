dbe88f58da7c7c37b728408256a40a42fe0f9155
mannonov0830 <kamronbekmannonov0830@gmail.com>
2026-04-26 11:40:17 +0500

    main

--- app.js
+++ app.js
@@ -1,55 +1,5 @@
-let display = document.getElementById('display')
-function bos(value) {
-    display.value += value
-}
-function hisoblash() {
-    let qiymat = display.value;
+let display = document.getElementById("display")
 
-    let son = "";
-    let natija = null; 
-    let amal = null;
-
-    for (let i = 0; i < qiymat.length; i++) {
-        let belgi = qiymat[i];
-
-       
-        if ((belgi >= "0" && belgi <= "9") || belgi === ".") {
-            son += belgi;
-        }
-
-        if (belgi === "+" || belgi === "-" || belgi === "*" || belgi === "/") {
-            let num = Number(son);
-
-            if (natija === null) {
-                natija = num; 
-            } else {
-                
-                if (amal === "+") natija += num;
-                if (amal === "-") natija -= num;
-                if (amal === "*") natija *= num;
-                if (amal === "/") natija /= num;
-            }
-
-            amal = belgi; 
-            son = "";     
-        }
-    }
-
-    
-    if (son !== "") {
-        let num = Number(son);
-        if (natija === null) {
-            natija = num;
-        } else {
-            if (amal === "+") natija += num;
-            if (amal === "-") natija -= num;
-            if (amal === "*") natija *= num;
-            if (amal === "/") natija /= num;
-        }
-    }
-
-    display.value = natija;
-}
-function tozalash(params) {
-    display.value = "";
+function bos(velue) {
+    display.velue += velue
 }
